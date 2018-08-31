abe397b7e5d361ed4710d5b4b242666830ea7fc3
Henry <henry@Henryde-iMac.local>
2018-08-31 11:48:42 +0800

    Reinstall babel packages and also reset the babelrc file because of the updated version

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,5 @@
 
 import num from './test';
 
-console.log(`I imported ${num} from another module called test.js`);
+let x = 23;
+console.log(`I imported ${num} from another module called test.js Variable x is ${x}`);
