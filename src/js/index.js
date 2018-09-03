103feb2f5e9c309dbe089fd5c2e86780796634ed
Henry <henry@Henryde-iMac.local>
2018-09-03 14:05:52 +0800

    Install axios for http request and test the food2fork api

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
 // Global app controller
+import axios from 'axios';
 
+async function getResults(query) {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const key = '3b0cd726bdd3855b1e26cbebe8815874';
+    
+    try {
+        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+getResults('pizza');
