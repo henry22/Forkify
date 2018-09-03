015ca5813b79aab7d933bde3ff977af8608c5f9c
Henry <henry@Henryde-iMac.local>
2018-09-03 15:13:41 +0800

    Create async function to get the data from the request and render on the UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,33 @@
 // Global app controller
 import Search from './models/Search';
 
-const search = new Search('pizza');
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked object
+ */
+const state = {};
 
-console.log(search);
-search.getResults();
+const controlSearch = async () => {
+    // 1. Get query from the view
+    const query = 'pizza';
+
+    if(query) {
+        // 2. New search object and add to the state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for results
+
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // 5. Render results on UI
+        console.log(state.search.results);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
