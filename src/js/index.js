b00d1bbe1f8393b963e3250dc04ce42a9d8a5f18
Henry <henry@Henryde-iMac.local>
2018-09-03 14:29:41 +0800

    Encapsulate the async function into the class inside the model, then call it from the index js

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,7 @@
 // Global app controller
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = "https://cors-anywhere.herokuapp.com/";
-    const key = '3b0cd726bdd3855b1e26cbebe8815874';
-    
-    try {
-        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch(error) {
-        console.log(error);
-    }
-}
+const search = new Search('pizza');
 
-getResults('pizza');
+console.log(search);
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = "https://cors-anywhere.herokuapp.com/";
+        const key = '3b0cd726bdd3855b1e26cbebe8815874';
+
+        try {
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.results = res.data.recipes;
+            console.log(this.results);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
