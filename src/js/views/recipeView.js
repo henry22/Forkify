914925724a188dd2b294e9ed02544e43cd4cb415
Henry <henry@Henryde-iMac.local>
2018-09-05 17:57:31 +0800

    Install fractional npm package and create a function to format the count number

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,15 +1,35 @@
 import { elements } from './base';
+import { Fraction } from 'fractional';
 
 export const clearRecipe = () => {
     elements.recipe.innerHTML = '';
 };
+
+const formatCount = count => {
+    if(count) {
+        // ex: 2.5 -> 2 1/2, 0.5 -> 1/2
+        const [int, dec] = count.toString().split('').map(el => parseInt(el, 10));
+
+        if(!dec) return count;
+
+        if(int === 0) {
+            const fractional = new Fraction(count);
+            return `${fractional.numerator}/${fractional.denominator}`;
+        } else {
+            const fractional = new Fraction(count - int);
+            return `${int} ${fractional.numerator}/${fractional.denominator}`;
+        }
+    }
+
+    return '?';
+}
 
 const createIngredient = ingredient => `
     <li class="recipe__item">
         <svg class="recipe__icon">
             <use href="img/icons.svg#icon-check"></use>
         </svg>
-        <div class="recipe__count">${ingredient.count}</div>
+        <div class="recipe__count">${formatCount(ingredient.count)}</div>
         <div class="recipe__ingredient">
             <span class="recipe__unit">${ingredient.unit}</span>
             ${ingredient.ingredient}
