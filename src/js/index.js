052c3a7ae0af855c003e6be3e23bcb6d073878bf
Henry <henry@Henryde-iMac.local>
2018-09-05 18:18:15 +0800

    Highlight the search item when clicking

--- src/js/index.js
+++ src/js/index.js
@@ -71,6 +71,9 @@ const controlRecipe = async () => {
         recipeView.clearRecipe();
         renderLoader(elements.recipe);
 
+        // Highlight selected item
+        if(state.search) searchView.highlightSelected(id);
+
         // Create new recipe object
         state.recipe = new Recipe(id);
 
