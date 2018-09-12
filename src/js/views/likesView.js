277c203940fdfe15e1d2cf8abdb96e4d60bf9379
Henry <henry@Henryde-iMac.local>
2018-09-12 16:21:14 +0800

    Fix toggle like button svg icon path

--- src/js/views/likesView.js
+++ src/js/views/likesView.js
@@ -4,7 +4,7 @@ import { limitRecipeTitle } from './searchView';
 export const toggleLikeBtn = isLiked => {
     const iconStr = isLiked ? 'icon-heart' : 'icon-heart-outlined';
 
-    document.querySelector('.recipe__love use').setAttribute('href', `img/icons.svg#${iconStr}`);
+    document.querySelector('.recipe__love use').setAttribute('href', `./dist/img/icons.svg#${iconStr}`);
 };
 
 export const toggleLikeMenu = numLikes => {
